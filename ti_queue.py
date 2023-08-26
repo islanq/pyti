001d63b29f84e92421655329cc6eacc5ed512699
Jared Dyet <jdyet@asu.edu>
2023-08-25 20:44:46 -0700

    datascructs added & updated notebook w/ converter

---
+++ ti_queue.py
@@ -0,0 +1,27 @@
+class Queue:
+  def __init__(self):
+    self.items = []
+
+  def is_empty(self):
+    return len(self.items) == 0
+
+  def enqueue(self, item):
+    self.items.append(item)
+
+  def dequeue(self):
+    if not self.is_empty():
+      return self.items.pop(0)
+    else:
+      raise IndexError("Queue is empty.")
+
+  def peek(self):
+    if not self.is_empty():
+      return self.items[0]
+    else:
+      raise IndexError("Queue is empty.")
+
+  def size(self):
+    return len(self.items)
+
+  def __repr__(self):
+    return "Queue(" + ", ".join(map(str, self.items)) + ")"
