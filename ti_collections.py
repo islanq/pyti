baf02be63291f91c189f4f4315ff8afecc30a0c3
Jared Dyet <jdyet@asu.edu>
2023-09-07 02:35:32 -0700

    ti_interop -> ti_collections, and new ti_interop

---
+++ ti_collections.py
@@ -0,0 +1,204 @@
+import sys
+
+if sys.platform == 'win32':
+    from lib.polyfill import is_numeric, is_digit
+    from lib.frac import Frac
+    tiexec = None
+else:
+    from ti_system import readST, writeST
+    from eval_expr import eval_expr, call_func
+    from ti_interop import tiexec
+    from polyfill import is_numeric, is_digit
+
+
+class TiReadWriteException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(message)
+
+class TiCollections:
+    
+    def __init__(self):
+        for method_name in dir(TiCollections):
+            method = getattr(TiCollections, method_name)
+            if callable(method) and isinstance(method, staticmethod):
+                method = method.__func__  # Get the actual function
+                setattr(self, method_name, method.__get__(self))
+        
+    
+    def save_ti_var(name, value):
+        try: 
+            writeST(name, value)
+        except TiReadWriteException as e:
+            print("error saving variable: {}".format(e.message))
+    
+    def load_ti_var(name):
+        try:
+            return readST(name)
+        except TiReadWriteException as e:
+            print("error loading variable: {}".format(e.message))
+    
+    def tiexec(self, cmd_str_or_func_name, *args):
+        return tiexec(cmd_str_or_func_name, *args)
+
+    @staticmethod
+    def is_ti_list(ti_list_str) -> bool:
+        if not isinstance(ti_list_str, str):
+            return False
+        if not (ti_list_str.startswith('{') and ti_list_str.endswith('}')):
+            return False
+        return all(s not in ti_list_str for s in [', ', ':'])
+    
+    @staticmethod
+    def is_ti_mat(ti_mat_str) -> bool:
+        # ti mat will never contain ', ' or ':'
+        if not isinstance(ti_mat_str, str):
+            return False
+        if not ti_mat_str.startswith('[['):
+            return False
+        if not ti_mat_str.endswith(']]'):
+            return False
+        return all(s not in ti_mat_str for s in [', ', ':'])
+       
+    @staticmethod
+    def is_ti_row_vec(ti_mat_str) -> bool:
+        return TiCollections.is_ti_mat(ti_mat_str) and not '][' in ti_mat_str 
+    
+    @staticmethod
+    def is_ti_col_vec(ti_mat_str) -> bool:
+        if not (TiCollections.is_ti_mat(ti_mat_str) and '][' in ti_mat_str):
+            return False
+        return TiCollections.is_py_col_vec(TiCollections.to_py_mat(ti_mat_str))
+       
+    @staticmethod
+    def is_py_list(py_list) -> bool:
+        return isinstance(py_list, list)
+    
+    @staticmethod
+    def is_py_mat(py_list) -> bool:
+        if not isinstance(py_list, list): # it's not a list
+            return False
+        if len(py_list) == 0: # it contains no rows, therefore not a matrix
+            return False
+        if not all(isinstance(row, list) for row in py_list): # all list rows
+            return False
+        return all(len(row) == len(py_list[0]) for row in py_list) # all rows == len
+    
+    @staticmethod
+    def to_ti_list(py_list) -> str:
+        if TiCollections.is_ti_list(py_list):
+            return py_list
+        return "{" + ",".join(map(str, py_list)) + "}"
+    
+    @staticmethod
+    def to_ti_mat(py_list) -> str:
+        if TiCollections.is_ti_mat(py_list):
+            return py_list
+        return "[" + "".join(map(lambda row: TiCollections.to_ti_list(row).replace('{', '[').replace('}', ']'), py_list)) + "]"
+    
+    @staticmethod
+    def to_py_list(ti_list_str) -> list:
+        if isinstance(ti_list_str, list):
+            return ti_list_str
+        if TiCollections.is_ti_list(ti_list_str): 
+            return [float(x.replace("−", "-")) if is_numeric(x) else int(x.replace("−", "-")) if is_digit(x) else x.strip() for x in ti_list_str[1:-1].split(",")]
+
+    @staticmethod
+    def is_reg_py_list(py_list) -> bool:
+        if not isinstance(py_list, list):
+            return False
+        return not all(isinstance(item, list) for item in py_list)
+    
+    @staticmethod
+    def to_py_mat(ti_mat_str) -> list:
+        if isinstance(ti_mat_str, list):
+            return ti_mat_str
+        if not TiCollections.is_ti_mat(ti_mat_str):
+            raise ValueError("Invalid ti matrix string: {}".format(ti_mat_str))
+        if not '][' in ti_mat_str:
+            ti_mat_str = ti_mat_str[2:-2]
+            return [[float(x.replace("−", "-")) if is_numeric(x) else int(x.replace("−", "-")) if is_digit(x) else x.strip() for x in ti_mat_str.split(",")]]
+        else:
+            return [[float(x.replace("−", "-")) if is_numeric(x) else int(x.replace("−", "-")) if is_digit(x) else x.strip() for x in row.split(",")] for row in ti_mat_str[2:-2].split("][")]
+
+    @staticmethod
+    def to_ti_col_vec(matrix) -> str:
+        if TiCollections.is_ti_col_vec(matrix):
+            return matrix
+        return TiCollections.to_ti_mat(TiCollections.to_py_col_vec(matrix))
+    
+    @staticmethod
+    def to_ti_row_vec(matrix) -> str:
+        if TiCollections.is_ti_row_vec(matrix):
+            return matrix
+        return TiCollections.to_ti_mat(TiCollections.to_py_row_vec(matrix))
+    
+    @staticmethod
+    def to_py_col_vec(matrix) -> list:
+        if TiCollections.is_py_col_vec(matrix):
+            return matrix
+        elif TiCollections.is_reg_py_list(matrix):
+            return [[item] for item in matrix]
+        elif TiCollections.is_ti_list(matrix):
+            return TiCollections.to_py_col_vec(TiCollections.to_py_list(matrix))
+        elif TiCollections.is_py_row_vec(matrix):
+            return TiCollections.to_py_col_vec(TiCollections.flatten(matrix))
+        elif TiCollections.is_ti_row_vec(matrix):
+            return TiCollections.to_py_col_vec(TiCollections.to_py_mat(matrix))
+        elif TiCollections.is_py_mat(matrix):
+            return TiCollections.to_py_col_vec(TiCollections.flatten(matrix))
+        raise ValueError("Cannot convert to column vec: {}".format(matrix))
+    
+    @staticmethod
+    def to_py_row_vec(matrix) -> list:
+        if TiCollections.is_py_row_vec(matrix):
+            return matrix
+        elif TiCollections.is_reg_py_list(matrix):
+            return [matrix]
+        elif TiCollections.is_ti_list(matrix):
+            return TiCollections.to_py_row_vec(TiCollections.to_py_list(matrix))
+        elif TiCollections.is_py_col_vec(matrix) or TiCollections.is_ti_col_vec(matrix):
+            return TiCollections.to_py_row_vec(TiCollections.flatten(matrix))
+        elif TiCollections.is_ti_row_vec(matrix):
+            return TiCollections.to_py_row_vec(TiCollections.to_py_mat(matrix))
+        elif TiCollections.is_py_mat(matrix):
+            return TiCollections.to_py_row_vec(TiCollections.flatten(matrix))
+        raise ValueError("Cannot convert to row vec: {}".format(matrix))
+                       
+    @staticmethod
+    def is_py_row_vec(matrix) -> bool:
+        if isinstance(matrix, list):
+            return len(matrix) == 1 and isinstance(matrix[0], list)
+        return False
+    
+    @staticmethod
+    def is_py_col_vec(matrix) -> bool:
+        if isinstance(matrix, list):
+            return all(isinstance(row, list) and len(row) == 1 for row in matrix)
+        return False
+    
+    @staticmethod
+    def flatten(lst) -> list:
+        """Flatten a list of lists into a single list."""
+        return [item for sublist in lst for item in (TiCollections.flatten(sublist) if isinstance(sublist, list) else [sublist])]
+    
+    @staticmethod
+    def is_equality(expression: str) -> bool:
+        if not isinstance(expression, str):
+            return False
+        if "=" not in expression and "==" not in expression:
+            return False
+        if expression.count("=") > 1 or expression.count("==") > 1:
+            return False
+        if "==" in expression:
+            lhs, rhs = expression.split("==", 1)
+        else:
+            lhs, rhs = expression.split("=", 1)
+        return not (lhs.strip() == "" and rhs.strip() == "")
+
+
+    @staticmethod
+    def ti_list_to_mat(ti_list_str, elements_per_row = 1) -> str: 
+        if not TiCollections.is_ti_list(ti_list_str):
+            raise ValueError("Invalid ti list string: {}".format(ti_list_str))
+        return TiCollections.tiexec("list@>mat({}, {})".format(ti_list_str, elements_per_row))
--- ti_matrix.py
+++ ti_matrix.py
@@ -1,24 +1,26 @@
 import sys
+if sys.platform == 'win32':
+    exec(open('__init__.py').read())
 if sys.platform == 'TI-Nspire':
-    from inter import Interop
     from eval_expr import eval_expr, call_func
 else:
-    from ti_interop import TiInterop
-    call_func = TiInterop.call_func
-    eval_expr = TiInterop.eval_expr
+    call_func = TiCollections.call_func
+    eval_expr = TiCollections.eval_expr
     
-class TiMatrix(TiInterop):
+from ti_collections import TiCollections
+    
+class TiMatrix(TiCollections):
     
     def abs(self, py_matrix):
-        ti_matrix = TiInterop.to_ti_mat(py_matrix)
+        ti_matrix = TiCollections.to_ti_mat(py_matrix)
         result_str = call_func("abs", ti_matrix)
-        return TiInterop.to_py_mat(result_str)
+        return TiCollections.to_py_mat(result_str)
     
     def augment(self, py_matrix1, py_matrix2):
-        ti_matrix1 = TiInterop.to_ti_mat(py_matrix1)
-        ti_matrix2 = TiInterop.to_ti_mat(py_matrix2)
+        ti_matrix1 = TiCollections.to_ti_mat(py_matrix1)
+        ti_matrix2 = TiCollections.to_ti_mat(py_matrix2)
         result_str = call_func("augment", ti_matrix1 + "," + ti_matrix2)
-        return TiInterop.to_py_mat(result_str)
+        return TiCollections.to_py_mat(result_str)
 
     def col_augment(self, py_matrix1, py_matrix2):
         """
@@ -28,10 +30,10 @@ class TiMatrix(TiInterop):
             `py_matrix2 is appended to py_matrix1 as new
             rows`. Does not alter py_matrix1 or py_matrix2
         """
-        ti_matrix1 = TiInterop.to_ti_mat(py_matrix1)
-        ti_matrix2 = TiInterop.to_ti_mat(py_matrix2)
+        ti_matrix1 = TiCollections.to_ti_mat(py_matrix1)
+        ti_matrix2 = TiCollections.to_ti_mat(py_matrix2)
         result_str = call_func("colAugment", ti_matrix1, ti_matrix2)
-        return TiInterop.to_py_mat(result_str)
+        return TiCollections.to_py_mat(result_str)
 
     def sub_mat(self, py_matrix, start_row=None, start_col=None, end_row=None, end_col=None):
         """
@@ -39,10 +41,10 @@ class TiMatrix(TiInterop):
             Defaults: start_row=1, start_col=1,
             end_row=last row, end_col=last column
         """
-        ti_matrix = TiInterop.to_ti_mat(py_matrix)
+        ti_matrix = TiCollections.to_ti_mat(py_matrix)
         args = [str(arg) for arg in [start_row, start_col, end_row, end_col] if arg is not None]
         result_str = call_func("subMat", ti_matrix + "," + ",".join(args))
-        return TiInterop.to_py_mat(result_str)
+        return TiCollections.to_py_mat(result_str)
 
     def construct_mat(expr, var1: str, var2:str, num_rows: int, num_cols:int):
         
@@ -59,11 +61,11 @@ class TiMatrix(TiInterop):
             is incremented from `1` through num_cols.        
         """
         result_str = call_func("constructMat", expr, var1, var2, num_rows, num_cols)
-        return TiInterop.to_py_mat(result_str)
+        return TiCollections.to_py_mat(result_str)
     
     def rand_mat(self, num_rows: int, num_cols: int): # working
         result_str = call_func("randMat", num_rows, num_cols)
-        return TiInterop.to_py_mat(result_str)    
+        return TiCollections.to_py_mat(result_str)    
 
     def comulative_sum(self, py_matrix):
         """
@@ -77,9 +79,9 @@ class TiMatrix(TiInterop):
             resulting list or matrix. For more
             information on empty elements, see page
         """
-        ti_matrix = TiInterop.to_ti_mat(py_matrix)
+        ti_matrix = TiCollections.to_ti_mat(py_matrix)
         result_str = call_func("cumulativeSum", ti_matrix)
-        return TiInterop.to_py_mat(result_str)
+        return TiCollections.to_py_mat(result_str)
     
     def eig_vc(self, py_matrix):
         """
@@ -94,9 +96,9 @@ class TiMatrix(TiInterop):
             if V = [x_1, x_2, … , x_n]
             then x_1^2 + x_2^2 + … + x_n^2 = 1
         """
-        ti_matrix = TiInterop.to_ti_mat(py_matrix)
+        ti_matrix = TiCollections.to_ti_mat(py_matrix)
         result_str = call_func("eigVc", ti_matrix)
-        return TiInterop.to_py_mat(result_str)
+        return TiCollections.to_py_mat(result_str)
         
     def eig_vl(self, py_matrix):
         """
@@ -111,12 +113,12 @@ class TiMatrix(TiInterop):
             if V = [x_1, x_2, … , x_n]
             then x_1^2 + x_2^2 + … + x_n^2 = 1
         """
-        ti_matrix = TiInterop.to_ti_mat(py_matrix)
+        ti_matrix = TiCollections.to_ti_mat(py_matrix)
         result_str = call_func("eigVl", ti_matrix)
-        return TiInterop.to_py_mat(result_str)    
+        return TiCollections.to_py_mat(result_str)    
     
     def det(self, py_matrix, tol=None):
-        ti_matrix = TiInterop.to_ti_mat(py_matrix)
+        ti_matrix = TiCollections.to_ti_mat(py_matrix)
         if tol is not None:
             result_str = call_func("det", ti_matrix + "," + str(tol))
         else:
@@ -125,12 +127,12 @@ class TiMatrix(TiInterop):
 
     def diag(self, py_list_or_matrix):
         if isinstance(py_list_or_matrix[0], list):  # Assuming it's a matrix
-            ti_matrix = TiInterop.to_ti_mat(py_list_or_matrix)
+            ti_matrix = TiCollections.to_ti_mat(py_list_or_matrix)
             result_str = call_func("diag", ti_matrix)
         else:  # Assuming it's a list
-            ti_list = TiInterop.to_ti_list(py_list_or_matrix)
+            ti_list = TiCollections.to_ti_list(py_list_or_matrix)
             result_str = call_func("diag", ti_list)
-        return TiInterop.to_py_mat(result_str)
+        return TiCollections.to_py_mat(result_str)
     
     def row_add(self, py_matrix, sum_with_row, sum_to_row):
         """
@@ -139,20 +141,20 @@ class TiMatrix(TiInterop):
             sum_with_row and sum_to_row.
 
         """
-        ti_matrix = TiInterop.to_ti_mat(py_matrix)
+        ti_matrix = TiCollections.to_ti_mat(py_matrix)
         result_str = call_func("rowAdd", ti_matrix + "," + str(sum_with_row) + "," + str(sum_to_row))
-        return TiInterop.to_py_mat(result_str)
+        return TiCollections.to_py_mat(result_str)
     
     def row_sub(self, input_matrix, row_to_subtract, row_to_target, factor=1):
 
         row_subtract_values = self.get_row(input_matrix, row_to_subtract)
         row_subtract_target = self.get_row(input_matrix, row_to_target)
         
-        sub_matrix = TiInterop.to_ti_mat(row_subtract_values)
-        tar_matrix = TiInterop.to_ti_mat(row_subtract_target)
+        sub_matrix = TiCollections.to_ti_mat(row_subtract_values)
+        tar_matrix = TiCollections.to_ti_mat(row_subtract_target)
         
-        subtracted_result_str = self.exec("{}-{}*{}".format(tar_matrix, sub_matrix, factor))
-        subtracted_result_vec = TiInterop.to_py_mat(subtracted_result_str)[0]
+        subtracted_result_str = self.tiexec("{}-{}*{}".format(tar_matrix, sub_matrix, factor))
+        subtracted_result_vec = TiCollections.to_py_mat(subtracted_result_str)[0]
         
         result_py_matrix = [row for row in input_matrix]
         result_py_matrix[row_to_target - 1] = subtracted_result_vec
@@ -166,9 +168,9 @@ class TiMatrix(TiInterop):
             by `expr`.
         
         """
-        ti_matrix = TiInterop.to_ti_mat(matrix)
+        ti_matrix = TiCollections.to_ti_mat(matrix)
         result_str = call_func("mRow", str(expr) + "," + ti_matrix + "," + str(row_idx))
-        return TiInterop.to_py_mat(result_str)
+        return TiCollections.to_py_mat(result_str)
 
     def m_row_add(self, expr, matrix, mulexpr_row, update_row_idx):
         """
@@ -177,35 +179,35 @@ class TiMatrix(TiInterop):
             with:
             expr • mulexpr_row + update_row_idx
         """
-        ti_matrix = TiInterop.to_ti_mat(matrix)
+        ti_matrix = TiCollections.to_ti_mat(matrix)
         result_str = call_func("mRowAdd", str(expr) + "," + ti_matrix + "," + str(mulexpr_row) + "," + str(update_row_idx))
-        return TiInterop.to_py_mat(result_str)
+        return TiCollections.to_py_mat(result_str)
     
     def identity(self, size: int):
         result_str = call_func("identity", size)
-        return TiInterop.to_py_mat(result_str)
+        return TiCollections.to_py_mat(result_str)
 
     def col_norm(self, py_matrix):
-        ti_matrix = TiInterop.to_ti_mat(py_matrix)
+        ti_matrix = TiCollections.to_ti_mat(py_matrix)
         result_str = call_func("colNorm", ti_matrix)
         return result_str  # Assuming it returns a scalar value or expression
     
     def row_norm(self, py_matrix):
-        ti_matrix = TiInterop.to_ti_mat(py_matrix)
+        ti_matrix = TiCollections.to_ti_mat(py_matrix)
         result_str = call_func("rowNorm", ti_matrix)
         return result_str  # Assuming it returns a scalar value or expression
 
     def col_dim(self, py_matrix, ti_force = False):
         if not ti_force:
             return len(py_matrix[0])
-        ti_matrix = TiInterop.to_ti_mat(py_matrix)
+        ti_matrix = TiCollections.to_ti_mat(py_matrix)
         result_str = call_func("colDim", ti_matrix)
         return int(result_str)  # Assuming it returns a scalar value or expression   
 
     def row_dim(self, py_matrix, ti_force = False):
         if not ti_force:
             return len(py_matrix)
-        ti_matrix = TiInterop.to_ti_mat(py_matrix)
+        ti_matrix = TiCollections.to_ti_mat(py_matrix)
         result_str = call_func("rowDim", ti_matrix)
         return int(result_str)  # Assuming it returns a scalar value or expression
 
@@ -214,51 +216,179 @@ class TiMatrix(TiInterop):
             matrix = [row for row in py_matrix]
             matrix[row1 - 1], matrix[row2 - 1] = matrix[row2 - 1], matrix[row1 - 1]
             return matrix
-        ti_matrix = TiInterop.to_ti_mat(py_matrix)
+        ti_matrix = TiCollections.to_ti_mat(py_matrix)
         result_str = call_func("rowSwap", ti_matrix + "," + str(row1) + "," + str(row2))
         result_str = call_func("rowSwap", ti_matrix, row1, row2)
-        return TiInterop.to_py_mat(result_str)
+        return TiCollections.to_py_mat(result_str)
     
     def get_row(self, py_matrix, row_idx): 
-        ti_matrix = TiInterop.to_ti_mat(py_matrix)
-        result_str = self.exec("{}[{}]".format(ti_matrix, row_idx))
-        return TiInterop.to_py_mat(result_str)
+        ti_matrix = TiCollections.to_ti_mat(py_matrix)
+        result_str = self.tiexec("{}[{}]".format(ti_matrix, row_idx))
+        return TiCollections.to_py_mat(result_str)
 
     def get_col(self, py_matrix, col_idx):
-        ti_matrix = TiInterop.to_ti_mat(py_matrix)
+        ti_matrix = TiCollections.to_ti_mat(py_matrix)
         max_row = len(py_matrix)
         args = (ti_matrix, 1, col_idx, max_row, col_idx)
-        result_str = self.exec("subMat", *args)
-        return TiInterop.to_py_mat(result_str)
+        result_str = self.tiexec("subMat", *args)
+        return TiCollections.to_py_mat(result_str)
 
     def ref(self, py_matrix):
-        ti_matrix = TiInterop.to_ti_mat(py_matrix)
+        ti_matrix = TiCollections.to_ti_mat(py_matrix)
         result_str = call_func("ref", ti_matrix)
-        return TiInterop.to_py_mat(result_str)
+        return TiCollections.to_py_mat(result_str)
 
     def rref(self, py_matrix, tol=None):
-        ti_matrix = TiInterop.to_ti_mat(py_matrix)
+        ti_matrix = TiCollections.to_ti_mat(py_matrix)
         if tol is not None:
             result_str = call_func("rref", ti_matrix + "," + str(tol))
         else:
             result_str = call_func("rref", ti_matrix)
-        return TiInterop.to_py_mat(result_str)
+        return TiCollections.to_py_mat(result_str)
 
     def simult(self, py_coeff_matrix, const_col_vector, tol=None):
-        ti_coeff_matrix = TiInterop.to_ti_mat(py_coeff_matrix)
-        ti_const_vector = TiInterop.to_ti_mat(const_col_vector)
+        ti_coeff_matrix = TiCollections.to_ti_mat(py_coeff_matrix)
+        ti_const_vector = TiCollections.to_ti_mat(const_col_vector)
         if tol is not None:
             result_str = call_func("simult", ti_coeff_matrix + "," + ti_const_vector + "," + str(tol))
         else:
             result_str = call_func("simult", ti_coeff_matrix + "," + ti_const_vector)
-        return TiInterop.to_py_mat(result_str)
+        return TiCollections.to_py_mat(result_str)
 
     def transpose(self, py_matrix):
-        ti_matrix = TiInterop.to_ti_mat(py_matrix)
+        ti_matrix = TiCollections.to_ti_mat(py_matrix)
         result_str = eval_expr("{}@t".format(ti_matrix))
-        return TiInterop.to_py_mat(result_str)
+        return TiCollections.to_py_mat(result_str)
     
     def inverse(self, py_matrix):
-        ti_matrix = TiInterop.to_ti_mat(py_matrix)
+        ti_matrix = TiCollections.to_ti_mat(py_matrix)
         result_str = eval_expr("({}^-1)".format(ti_matrix))
-        return TiInterop.to_py_mat(result_str)
+        return TiCollections.to_py_mat(result_str)
+    
+    """
+    
+    
+    def log_matrix_operation(operation):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if operation == "swap":
+                print(f"r{args[1]} <-> r{args[2]}")
+            elif operation == "multiply":
+                print(f"r{args[1]} <- r{args[1]} * {args[2]}")
+            elif operation == "add":
+                print(f"r{args[1]} <- r{args[1]} + r{args[2]}")
+            elif operation == "m_row_add":
+                print(f"r{args[1]} <- r{args[1]} + {args[2]} * r{args[3]}")
+            elif operation == "sub":
+                print(f"r{args[1]} <- r{args[1]} - r{args[2]}")
+            elif operation == "div":
+                print(f"r{args[1]} <- r{args[1]} / {args[2]}")
+            # Add more operation types if needed
+            
+            result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator
+
+# Usage
+
+@log_matrix_operation("swap")
+def swap_rows(matrix, row1, row2):
+    matrix[row1], matrix[row2] = matrix[row2], matrix[row1]
+    return matrix
+
+@log_matrix_operation("multiply")
+def multiply_row(matrix, row, scalar):
+    matrix[row] = [x * scalar for x in matrix[row]]
+    return matrix
+
+@log_matrix_operation("add")
+def add_rows(matrix, target_row, source_row):
+    matrix[target_row] = [x + y for x, y in zip(matrix[target_row], matrix[source_row])]
+    return matrix
+
+@log_matrix_operation("m_row_add")
+def m_row_add(matrix, target_row, scalar, source_row):
+    matrix[target_row] = [x + scalar * y for x, y in zip(matrix[target_row], matrix[source_row])]
+    return matrix
+
+@log_matrix_operation("sub")
+def sub_rows(matrix, target_row, source_row):
+    matrix[target_row] = [x - y for x, y in zip(matrix[target_row], matrix[source_row])]
+    return matrix
+
+@log_matrix_operation("div")
+def div_row(matrix, row, scalar):
+    matrix[row] = [x / scalar for x in matrix[row]]
+    return matrix
+
+# Test
+mat = [[1, 2], [3, 4]]
+
+swap_rows(mat, 0, 1)
+multiply_row(mat, 0, 2)
+add_rows(mat, 0, 1)
+m_row_add(mat, 0, 2, 1)
+sub_rows(mat, 0, 1)
+div_row(mat, 0, 2)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    """
+    
+    
+    
+    """
+    
+    
+    
+    def deduce_matrix_operation(initial_matrix, final_matrix):
+    if len(initial_matrix) != len(final_matrix):
+        return "The number of rows has changed. Cannot deduce operation."
+    if len(initial_matrix[0]) != len(final_matrix[0]):
+        return "The number of columns has changed. Cannot deduce operation."
+    
+    changed_row = None
+    changed_count = 0
+    
+    for i in range(len(initial_matrix)):
+        if initial_matrix[i] != final_matrix[i]:
+            changed_row = i
+            changed_count += 1
+            
+    if changed_count == 0:
+        return "No operation detected."
+    
+    if changed_count > 1:
+        return "Multiple rows have changed. Cannot deduce operation."
+    
+    initial_row = initial_matrix[changed_row]
+    final_row = final_matrix[changed_row]
+    
+    # Check for scalar multiplication
+    scalar = final_row[0] / initial_row[0] if initial_row[0] != 0 else None
+    if all(fr == ir * scalar for fr, ir in zip(final_row, initial_row)):
+        return f"Row {changed_row + 1} was multiplied by {scalar}."
+    
+    # Check for row addition
+    diff = [fr - ir for fr, ir in zip(final_row, initial_row)]
+    for i in range(len(initial_matrix)):
+        if i == changed_row:
+            continue
+        if all(d == ir for d, ir in zip(diff, initial_matrix[i])):
+            return f"Row {changed_row + 1} was replaced by the sum of row {changed_row + 1} and row {i + 1}."
+    
+    return "Could not deduce operation."
+
+    
+    
+    
+    
+    """
