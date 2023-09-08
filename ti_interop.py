eb2d84e5620f3236da7b688b855e2157865b71aa
Jared Dyet <jdyet@asu.edu>
2023-09-07 18:04:08 -0700

    general quality of life

--- ti_interop.py
+++ ti_interop.py
@@ -1,23 +1,39 @@
 import sys
+
+
 if sys.platform == 'win32':
-    exec(open('__init__.py').read())
+    # exec(open('__init__.py').read())
     readST = None
     writeST = None
     eval_expr = None
     call_func = None
 else: 
+    import ti_system
     from eval_expr import eval_expr, call_func
     from ti_system import readST, writeST
+    readst = readST
+    writest = writeST
     
+
 _assignment_operators =[':=', '→']
     
-class TiType:
-    pass
+def is_ti_type(obj) -> bool:
+    from ti_matrix import TiMatrix
+    from ti_list import TiList
+    from ti_expression import TiExpression
+    return any(isinstance(obj, cls) for cls in (TiMatrix, TiList, TiExpression))
+
+      
 
 class TiUninitializedVariableError(NameError):
     def __init__(self, message):
         self.message = message
         super().__init__(message)
+
+def clear():
+    """Clears the screen of the the python console"""
+    ti_system.disp_clr()
+    
 
 def _string_all_args(*args):
     lst = []
@@ -28,8 +44,20 @@ def _string_all_args(*args):
             lst.append(str(arg))
     return ", ".join(lst)
 
+def get_var_mat() -> 'TiMatrix':
+    """Returns the varmat as a TiMatrix object"""
+    from ti_matrix import TiMatrix
+    try:
+        return TiMatrix(readST("getvarinfo()"))
+    except Exception as e:
+        print("There was an error getting the varmat: {}".format(e))
+        return None
+
+def _remove_none_args(*args) -> tuple:
+    return tuple(arg for arg in args if arg is not None)
     
-def tiexec(cmd_str_or_func_name, *args, write=False):
+def tiexec(cmd_str_or_func_name, *args, write=False) -> any:
+    args = _remove_none_args(*args)
     if write:
         try:
             writeST(cmd_str_or_func_name, *args)
@@ -48,26 +76,29 @@ def tiexec(cmd_str_or_func_name, *args, write=False):
     try:
         return eval_expr(cmd_str_or_func_name)
     except TiUninitializedVariableError as err:
+        print("eval_expr: {}".format(err))
         pass
     try:
         return readST(cmd_str_or_func_name)
     except TiUninitializedVariableError as err:
+        print("readST: {}".format(err))
         pass
     try:
         return readST('expr("{}")'.format(cmd_str_or_func_name))
     except TiUninitializedVariableError as err:
+        print("readST(expr): {}".format(err))
         pass
     
-def _is_assignment_attempt(expr):
+def _is_assignment_attempt(expr) -> bool:
     return any(op in expr for op in _assignment_operators)
 
-def var_save(name, value):
+def var_save(name, value) -> bool:
     try:
         writeST(name, value)
     except:
         pass
 
-def clearAZ():
+def clearAZ() -> None:
     for c in range(97, 122):
         if get_var(chr(c)) is not None:
             del_var(chr(c))
@@ -105,3 +136,4 @@ def do_manual_assignment(command):
         rhs, lhs = command.split(_assignment_operators[1])
     del_var(lhs.strip())
     return var_save(lhs.strip(), rhs.strip())
+
