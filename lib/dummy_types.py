d21128476461ed83fa5e6ba72c51f697f71fb6cd
Jared Dyet <jdyet@asu.edu>
2023-09-09 15:35:36 -0700

    supports new classes

---
+++ lib/dummy_types.py
@@ -0,0 +1,70 @@
+class TiType:
+    pass
+
+
+class TiList(TiType):
+    pass
+
+
+class TiMat(TiType):
+    pass
+
+
+class TiVec(TiType):
+    pass
+
+
+class TiColVec(TiVec):
+    pass
+
+
+class TiRowVec(TiVec):
+    pass
+
+
+class PyType:
+    pass
+
+
+class PyList(PyType):
+    pass
+
+
+class PyMat(PyType):
+    pass
+
+
+class PyVec(PyType):
+    pass
+
+
+class PyColVec(PyVec):
+    pass
+
+
+class PyRowVec(PyVec):
+    pass
+
+
+class PyStrType:
+    pass
+
+
+class PyStrList(PyStrType):
+    pass
+
+
+class PyStrMat(PyStrType):
+    pass
+
+
+class PyStrVec(PyStrType):
+    pass
+
+
+class PyStrColVec(PyStrVec):
+    pass
+
+
+class PyStrRowVec(PyStrVec):
+    pass
