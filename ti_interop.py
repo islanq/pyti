0f87c779e1e8e68c2aa4a56462833040d4f1ce56
Jared Dyet <jdyet@asu.edu>
2023-09-09 19:56:27 -0700

    housekeeping

--- ti_interop.py
+++ ti_interop.py
@@ -7,33 +7,34 @@ if sys.platform == 'win32':
     writeST = None
     eval_expr = None
     call_func = None
-else: 
+else:
     import ti_system
     from eval_expr import eval_expr, call_func
     from ti_system import readST, writeST
     readst = readST
     writest = writeST
-    
 
-_assignment_operators =[':=', '→']
-    
+
+_assignment_operators = [':=', '→']
+
+
 def is_ti_type(obj) -> bool:
     from ti_matrix import TiMatrix
     from ti_list import TiList
     from ti_expression import TiExpression
     return any(isinstance(obj, cls) for cls in (TiMatrix, TiList, TiExpression))
 
-      
 
 class TiUninitializedVariableError(NameError):
     def __init__(self, message):
         self.message = message
         super().__init__(message)
 
+
 def clear():
     """Clears the screen of the the python console"""
     ti_system.disp_clr()
-    
+
 
 def _string_all_args(*args):
     lst = []
@@ -44,6 +45,7 @@ def _string_all_args(*args):
             lst.append(str(arg))
     return ", ".join(lst)
 
+
 def get_var_mat() -> 'TiMatrix':
     """Returns the varmat as a TiMatrix object"""
     from ti_matrix import TiMatrix
@@ -53,9 +55,11 @@ def get_var_mat() -> 'TiMatrix':
         print("There was an error getting the varmat: {}".format(e))
         return None
 
+
 def _remove_none_args(*args) -> tuple:
     return tuple(arg for arg in args if arg is not None)
-    
+
+
 def tiexec(cmd_str_or_func_name, *args, write=False) -> any:
     args = _remove_none_args(*args)
     if write:
@@ -88,9 +92,11 @@ def tiexec(cmd_str_or_func_name, *args, write=False) -> any:
     except TiUninitializedVariableError as err:
         print("readST(expr): {}".format(err))
         pass
-    
+
+
 def _is_assignment_attempt(expr) -> bool:
     return any(op in expr for op in _assignment_operators)
+
 
 def var_save(name, value) -> bool:
     if not isinstance(value, str):
@@ -100,20 +106,24 @@ def var_save(name, value) -> bool:
     except:
         pass
 
+
 def clearAZ() -> None:
     for c in range(97, 122):
         if get_var(chr(c)) is not None:
             del_var(chr(c))
-    
+
+
 def var_exists(name):
     return get_var(name) is not None
+
 
 def get_var(name):
     try:
         return readST("{}".format(name))
     except Exception:
         return None
-    
+
+
 def del_var(*names):
     for name in names:
         name = str(name) if not isinstance(name, str) else name
@@ -130,7 +140,8 @@ def del_var(*names):
             else:
                 print("del_var had an error it couldn't resolve: {}".format(err))
                 return False
-            
+
+
 def do_manual_assignment(command):
     if _assignment_operators[0] in command:
         lhs, rhs = command.split(_assignment_operators[0])
@@ -138,4 +149,3 @@ def do_manual_assignment(command):
         rhs, lhs = command.split(_assignment_operators[1])
     del_var(lhs.strip())
     return var_save(lhs.strip(), rhs.strip())
-
