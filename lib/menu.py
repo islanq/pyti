baa2d8ff27eb21927daa81731e092a5711d23a91
Jared Dyet <jdyet@asu.edu>
2023-09-17 17:03:36 -0700

    menu slightly improved

--- lib/menu.py
+++ lib/menu.py
@@ -2,6 +2,7 @@ from collections import namedtuple
 
 MenuItem = namedtuple('MenuItem', ['name', 'description', 'func'])
 
+
 class Menu:
     def __init__(self, title):
         self.title = title
@@ -13,6 +14,7 @@ class Menu:
     def display(self):
         while True:  # Keep displaying the menu until 'Exit' is chosen
             print("\n--- {} ---".format(self.title))
+
             for i, category in enumerate(self.categories, 1):
                 print("{}. {} - {}".format(i, category.name, category.description))
 
@@ -26,11 +28,52 @@ class Menu:
                 selected_category.display()
 
 
+class Menu2:
+    def __init__(self, title):
+        self.title = title
+        self.categories = []
+        self.current_page = 0
+
+    def register_category(self, category):
+        self.categories.append(category)
+
+    def display(self):
+        while True:
+            print("\n--- {} ---".format(self.name))
+            start_idx = self.current_page * 7
+            end_idx = min(start_idx + 7, len(self.items))
+
+            for i, item in enumerate(self.items[start_idx:end_idx], start_idx + 1):
+                print("{}. {} - {}".format(i, item.name, item.description))
+            
+            extra_options_start = len(self.items) if end_idx >= len(self.items) else end_idx + 1
+
+            if end_idx < len(self.items):
+                print(f"{extra_options_start}. Next")
+                extra_options_start += 1
+            
+            print(f"{extra_options_start}. Back")
+
+            choice = int(input("Choose an option: ")) - start_idx - 1
+            
+            if choice in [7, 6] and end_idx < len(self.items):
+                self.current_page = (self.current_page + 1) % ((len(self.items) + 6) // 7)
+            elif choice in [7, 6]:
+                self.current_page = (self.current_page - 1) % ((len(self.items) + 6) // 7)
+            elif choice in [6, 5]:
+                return  # Return to the previous menu
+            else:
+                selected_item = self.items[start_idx + choice]
+                selected_item.func()
+                self.current_page = 0
+
+
 class Category:
     def __init__(self, name, description):
         self.name = name
         self.description = description
         self.items = []
+        self.current_page = 0
 
     def register_item(self, item):
         self.items.append(item)
@@ -50,67 +93,44 @@ class Category:
                 selected_item = self.items[choice - 1]
                 selected_item.func()
 
+
 class CategoryItem:
     def __init__(self, name, description, func):
         self.name = name
         self.description = description
         self.func = func
 
+
 def example_func():
     print("Example function executed.")
-
-
-
-
-
 
 
 # Define categories and items within "Linear Algebra"
 linear_algebra_menu = Menu("Linear Algebra")
 
 # Vectors category
-vectors = Category("Vectors", "Operations and properties of vectors")
+vectors = Category("Vectors", "Operations and properties of vectors")# Registering the categories
+
+
 vectors.register_item(MenuItem("Vector Operations", "Addition, Scalar Multiplication", example_func))
 vectors.register_item(MenuItem("Dot Product", "Calculate dot product", example_func))
 vectors.register_item(MenuItem("Cross Product", "Calculate cross product", example_func))
-vectors.register_item(MenuItem("Vector Projections", "Find projections of vectors", example_func))
+vectors.register_item(MenuItem("Vector Projections","Find projections of vectors", example_func))
 
 # Matrices category
 matrices = Category("Matrices", "Operations and properties of matrices")
+
 matrices.register_item(MenuItem("Matrix Operations", "Addition, Multiplication", example_func))
 matrices.register_item(MenuItem("Determinants", "Calculate determinants", example_func))
 matrices.register_item(MenuItem("Inverses", "Find matrix inverses", example_func))
-matrices.register_item(MenuItem("Transpose", "Find the transpose of a matrix", example_func))
-
-# Registering the categories
+matrices.register_item(MenuItem("Transpose1", "Find the transpose of a matrix", example_func))
+matrices.register_item(MenuItem("Transpose2", "Find the transpose of a matrix", example_func))
+matrices.register_item(MenuItem("Transpose3", "Find the transpose of a matrix", example_func))
+matrices.register_item(MenuItem("Transpose4", "Find the transpose of a matrix", example_func))
+matrices.register_item(MenuItem("Transpose5", "Find the transpose of a matrix", example_func))
+matrices.register_item(MenuItem("Transpose6", "Find the transpose of a matrix", example_func))
+matrices.register_item(MenuItem("Transpose7", "Find the transpose of a matrix", example_func))
 linear_algebra_menu.register_category(vectors)
 linear_algebra_menu.register_category(matrices)
+linear_algebra_menu.display()
 
-# Display the main Linear Algebra menu to show the categories and items
-# For demonstration purposes, let's display only the "Vectors" and "Matrices" categories and their items
-
-linear_algebra_menu.display()  # Uncomment this line to run the menu (requires interactive session)
-
-#print("Linear Algebra menu, categories, and items are set up. Uncomment the last line to run in an interactive session.")
-# def main():
-#     main_menu = Menu("Main Menu")
-
-#     category1 = Category("Category 1", "First Category")
-#     category2 = Category("Category 2", "Second Category")
-
-#     item1 = CategoryItem("Option 1", "Description 1", example_func)
-#     item2 = CategoryItem("Option 2", "Description 2", example_func)
-
-#     category1.register_item(item1)
-#     category1.register_item(item2)
-
-#     main_menu.register_category(category1)
-#     main_menu.register_category(category2)
-
-#     while True:
-#         result = main_menu.display()
-#         if result == "Exit":
-#             break
-
-# if __name__ == "__main__":
-#     main()
